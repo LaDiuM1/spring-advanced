589e5e177c4b6cd8258332bbd748b63fffb943d9
LaDiuM1 <ladiumkr@gamil.com>
2025-08-14 08:52:05 +0900

    [로그 추적 기능 - v4] strategy 패턴 테스트 예제 코드 추가
    - 로그 추적 기능 v4는 전략 패턴 적용 예정
    - 전략 패턴의 이해와 사용 방식을 테스트 코드로 작성

---
+++ src/test/java/study/spring_advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,62 @@
+package study.spring_advanced.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import study.spring_advanced.trace.strategy.code.strategy.ContextV1;
+import study.spring_advanced.trace.strategy.code.strategy.StrategyLogic1;
+import study.spring_advanced.trace.strategy.code.strategy.StrategyLogic2;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 구현체 기반 전략 패턴
+     */
+    @Test
+    void strategyV1() {
+        ContextV1 context1 = new ContextV1(new StrategyLogic1());
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new StrategyLogic2());
+        context2.execute();
+    }
+
+    /**
+     * 익명 클래스 기반 전략 패턴
+     */
+    @Test
+    void strategyV2() {
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
+        context2.execute();
+    }
+
+}
---
+++ src/test/java/study/spring_advanced/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,7 @@
+package study.spring_advanced.trace.strategy.code.strategy;
+
+public interface Strategy {
+
+    void call();
+
+}
