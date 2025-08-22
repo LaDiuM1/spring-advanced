b9ea6e62b98a919e8faafbf7544a97981b12212c
LaDiuM1 <ladiumkr@gamil.com>
2025-08-22 09:35:17 +0900

    [Proxy - test] 프록시 패턴 테스트 예제 코드 추가
    - GOF 프록시 패턴을 캐시 데이터 프록시로 표현한 예제 코드 추가

---
+++ src/test/java/study/proxy/pureproxy/proxy/code/SubjectCacheProxy.java
@@ -0,0 +1,30 @@
+package study.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubjectCacheProxy implements Subject {
+
+    private final Subject target;
+    private String cachedData = null;
+
+    public SubjectCacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        return getData();
+    }
+
+    private String getData() {
+        if (cachedData == null) {
+            cachedData = target.operation();
+        } else {
+            log.info("캐시된 객체 호출");
+        }
+
+        return cachedData;
+    }
+
+}
