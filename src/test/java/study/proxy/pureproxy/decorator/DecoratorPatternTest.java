2c06c9e5f7a54af16b0829d7a7f0fbaac5370db6
LaDiuM1 <ladiumkr@gamil.com>
2025-08-25 09:31:35 +0900

    [Proxy - test] 데코레이터 패턴 테스트 예제 코드 추가
    - GOF 데코레이터 패턴으로 실행 시간 측정, 메시지 추가 기능 구현
    - TimeDecorator로 실행 시간 측정 데코레이터 기능 구현
    - MessageDecorator로 메시지 추가 데코레이터 기능 구현

---
+++ src/test/java/study/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,24 @@
+package study.proxy.pureproxy.decorator;
+
+import org.junit.jupiter.api.Test;
+import study.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import study.proxy.pureproxy.decorator.code.MessageDecorator;
+import study.proxy.pureproxy.decorator.code.RealComponent;
+import study.proxy.pureproxy.decorator.code.TimeDecorator;
+
+public class DecoratorPatternTest {
+
+    @Test
+    void no_decorator() {
+        DecoratorPatternClient client = new DecoratorPatternClient(new RealComponent());
+        client.execute();
+    }
+
+    @Test
+    void decorator() {
+        MessageDecorator messageDecorator = new MessageDecorator(new RealComponent());
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
---
+++ src/test/java/study/proxy/pureproxy/decorator/code/Component.java
@@ -0,0 +1,6 @@
+package study.proxy.pureproxy.decorator.code;
+
+public interface Component {
+
+    String operation();
+}
