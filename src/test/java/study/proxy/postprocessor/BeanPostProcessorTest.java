442c85fa2aef9401406565c538cedd50d107e3ce
LaDiuM1 <ladiumkr@gamil.com>
2025-09-29 09:17:07 +0900

    [Proxy - test] 빈 후처리기 사용 예제 코드 추가
    - BeanPostProcessor 구현하여 빈 후처리 기능 테스트

---
+++ src/test/java/study/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,61 @@
+package study.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class BeanPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // beanA 호출 시 빈 후처리기에 의해 B 클래스로 교체되었는지 확인
+        Assertions.assertThat(ac.getBean("beanA")).isInstanceOf(B.class);
+    }
+
+    @Slf4j
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor aToBPostProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={}, bean={}", beanName, bean);
+            if(bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+}
