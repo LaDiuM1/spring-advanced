03bb0269e14a3d872e2be6a62eb21722e7913087
LaDiuM1 <ladiumkr@gamil.com>
2025-10-13 09:30:24 +0900

    [리팩토링] 컨테이너 베이스 스캔 클래스 변경
    - ProxyApplication.java 내 scanBasePackages 제거
    - AppV4Config.java에 scanBasePackages "study.spring_advanced.proxy.app.v4" 추가
    - packageLogTracePostProcessor 메서드 적용 위치 "study.spring_advanced.proxy.app.v4"로 변경

--- src/main/java/study/spring_advanced/proxy/config/v4/AppV4Config.java
+++ src/main/java/study/spring_advanced/proxy/config/v4/AppV4Config.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import study.spring_advanced.proxy.config.v3.LogTraceAdvice;
 import study.spring_advanced.proxy.config.v4.postprocessor.PackageLogTracePostProcessor;
@@ -12,11 +14,14 @@ import study.spring_advanced.proxy.trace.logtrace.LogTrace;
 
 @Slf4j
 @Configuration
+@ComponentScan(basePackages = {
+        "study.spring_advanced.proxy.app.v4",
+})
 public class AppV4Config {
 
     @Bean
     public PackageLogTracePostProcessor packageLogTracePostProcessor(LogTrace logTrace) {
-        return new PackageLogTracePostProcessor("study.spring_advanced.proxy.app", getAdvisor(logTrace));
+        return new PackageLogTracePostProcessor("study.spring_advanced.proxy.app.v4", getAdvisor(logTrace));
     }
 
     private Advisor getAdvisor(LogTrace logTrace) {
