a9901df00b343fbf5039560d85c45809be6d774a
LaDiuM1 <ladiumkr@gamil.com>
2025-10-17 09:25:29 +0900

    [AOP] 스프링 AOP 구현을 위한 루트 패키지(프로젝트) 생성
    - AOP 루트 패키지 생성 후 SpringBootApplication 적용 클래스 생성
    - Order App 생성 -> OrderService, OrderRepository
    - 기본 동작 확인용 테스트 클래스 작성

---
+++ src/main/java/study/spring_advanced/aop/app/order/OrderRepository.java
@@ -0,0 +1,17 @@
+package study.spring_advanced.aop.app.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+public class OrderRepository {
+
+    public void save(String itemId) {
+        log.info("[orderRepository] 실행");
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("저장 예외 발생");
+        }
+    }
+
+}
